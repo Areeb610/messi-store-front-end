707bc3eb04218b317e77587ec1b0e0a99751d203
Areeb610 <areebbutt610@gmail.com>
2023-11-25 15:21:10 +0500

    Backend Setup Completed

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,41 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const User = require('./models/users');
 
 const app = express();
 const port = 8080;
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://areebbutt610:CTXu5lNP0Zp1QNtu@cluster0.u68wt69.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://areebbutt610:CTXu5lNP0Zp1QNtu@cluster0.u68wt69.mongodb.net/MESSI_STORE', { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Define your routes and middleware here
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+
+});
+
+app.get('/add-user', (req, res) => {
+  const user = new User({
+    username: 'areebbutt610',
+    password: 'password',
+    email: 'areeb@gmail.com',
+    firstName: 'Areeb',
+    lastName: 'Butt',
+    role: 'admin',
+  });
+  user.save().then((result) => {
+    res.send(result);
+  });
+}
+);
+
+app.get('/all-users', (req, res) => {
+  User.find().then((result) => {
+    res.send(result);
+  });
+}
+);
+
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
